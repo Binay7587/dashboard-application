2c0796851ed2b98f427fc099c63247937a6f00ab
Binay Karki <Binay7587@gmail.com>
2025-02-19 20:32:50 +0545

    feat: implemented user list with pagination and search functionality

--- src/components/Table/TableSearch.tsx
+++ src/components/Table/TableSearch.tsx
@@ -1,4 +1,4 @@
-const TableSearch = ({ search, setSearch }: { search: string, setSearch: (search: string) => void }) => {
+const TableSearch = ({ search, setSearch, placeholder }: { search: string, setSearch: (search: string) => void, placeholder: string }) => {
   return (
     <div className="relative rounded-md">
       <input
@@ -8,7 +8,7 @@ const TableSearch = ({ search, setSearch }: { search: string, setSearch: (search
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         className="block w-[250px] rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 dark:text-white ring-1 ring-inset ring-gray-300 dark:ring-gray-700 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-black dark:focus:ring-white focus:outline-none sm:text-sm sm:leading-6 bg-white dark:bg-black"
-        placeholder="Search products"
+        placeholder={placeholder}
       />
     </div>
   )
